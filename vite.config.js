b3451afc8f52defe1ebe47bde4f2ca5efd8e1e30
lambert.bruyas <lambert.bruyas@ekimetrics.com>
2024-11-22 18:13:58 +0100

    debug github page

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/image-match-game/',
   plugins: [react()],
   server: {
-    host: true, // Needed for mobile testing
-    port: 3000,
-  },
+        host: true, // Needed for mobile testing
+  }
 })
